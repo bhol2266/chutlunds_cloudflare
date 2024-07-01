5adf38294d62cc35a76f018410c7b9f4714adc92
bhola <bhola2266@gmail.com>
2024-07-01 07:25:17 +0545

    dsfsdaf

--- spangbang.js
+++ spangbang.js
@@ -1,14 +1,65 @@
 
 import cheerio from 'cheerio';
-import { Scrape_Video_Item } from './config/Scrape_Video_Item';
+import fs  from'fs';
+
+
+
+
+
+ function Scrape_Video_Item($) {
+
+
+    const finalDataArray = [];
+
+    $('.main_results .video-item').each((i, el) => {
+
+        const thumbnail = $(el).find('picture img').attr('data-src');
+        const title = $(el).find('picture img').attr('alt');
+        const duration = $(el).find('.l').text();
+
+        const statsText = $(el).find('.stats').text();
+        const likePercentage = statsText.substring(statsText.indexOf("%") - 4, statsText.indexOf("%") + 1).trim();
+        const views = statsText.substring(0, statsText.indexOf("%") - 4).trim();
+
+        const previewVideo = $(el).find('picture img').attr('data-preview');
+        const href = `https://spankbang.com${$(el).find('a').attr('href')}`;
+
+        if (href !== undefined && previewVideo !== undefined && !thumbnail.includes("//assets.sb-cd.com")) {
+            finalDataArray.push({
+                thumbnail: thumbnail,
+                title: title,
+                duration: duration,
+                likePercentage: likePercentage,
+                views: views,
+                previewVideo: previewVideo,
+                href: href,
+            });
+        }
+    });
+
+    return finalDataArray;
+}
+
+
+
+
+
  const scrapeVideos = async () => {
     var finalDataArray = []
     var pages = []
 
 
-    const response = await fetch("https://spankbang.party/s/asian/?o=trending")
+    const response = await fetch("https://spankbang.party/s/chubby/")
     const body = await response.text();
     const $ = cheerio.load(body)
+
+    fs.writeFile('index.html', body, (err) => {
+        if (err) {
+            console.error('Error writing to file', err);
+        } else {
+            console.log('HTML saved to index.html');
+        }
+    });
 
 
     finalDataArray= Scrape_Video_Item($)
